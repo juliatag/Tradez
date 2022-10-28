384da23d256641fc477148a84ee895585f5ccb1b
karizmaj <julieta.jabbot@gmail.com>
2022-10-28 14:11:55 -0400

    Initial commit

---
+++ src/test/java/com/tradez/TradezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
