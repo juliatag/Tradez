b8b6590a0d17a23e366813c8fc6a009e5798ffe3
kim <kim1ee@hotmail.com>
2022-11-04 00:12:55 +0800

    add and update the listing

--- src/main/java/com/tradez/controllers/ListingController.java
+++ src/main/java/com/tradez/controllers/ListingController.java
@@ -1,10 +1,8 @@
 package com.tradez.controllers;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,27 +26,22 @@ public class ListingController {
 
 	@GetMapping("/createListing")
 	public String createListingPage(Model model) {
-		model.addAttribute("listing", new Listing());
-		setupModel(model);
-		return "new_listing";
-	}
-	
-	@PostMapping("/createListing")
-	public String createListing(Listing listing, @RequestPart MultipartFile imgFile) throws IOException {
-		this.listingService.save(listing, imgFile);
-		return "redirect:/dashboard";
+		setupModel(model,"Add Listing",new Listing());
+		return "listing_form";
 	}
 	
 	@GetMapping("/updateListing/{id}")
 	public String updateListingPage(@PathVariable("id") Long id,Model model) {
-		model.addAttribute("listing", this.listingService.findById(id));
-		setupModel(model);
-		return "update_listing";
+		setupModel(model,"Edit Listing",this.listingService.findById(id));
+		return "listing_form";
 	}
 	
-	@PostMapping("/updateListing")
-	public String updateListing(Listing listing, @RequestPart MultipartFile imgFile) throws IOException {
-		this.listingService.update(listing, imgFile);
+	@PostMapping("/saveListing")
+	public String saveListing(Listing listing, @RequestPart MultipartFile imgFile) throws IOException {
+		if(listing.getId() == null)
+		  this.listingService.save(listing, imgFile);
+		else
+		  this.listingService.update(listing, imgFile);	
 		return "redirect:/dashboard";
 	}
 	
@@ -60,21 +52,16 @@ public class ListingController {
 		return "redirect:/dashboard";
 	}
 
-//	@RequestMapping("/dashboard")
-//	public String dashboardPage(HttpServletRequest request, Model model) {
-//
-//		model.addAttribute("listings", this.listingService.findByUsername());
-//		return "dashboard";
-//	}
-	
 	@GetMapping(value="/loadListingImage/{id}",produces = MediaType.ALL_VALUE)
 	@ResponseBody
 	public byte[] loadImageById(@PathVariable("id") Long id) throws IOException {
 		return this.listingService.findImageById(id);
 	}	
 	
-	
-	private void setupModel(Model model) {
+	private void setupModel(Model model,String title,Listing listing) {
+		model.addAttribute("listing", listing);
+		model.addAttribute("title", title);
+		
 		model.addAttribute("categories", this.listingService.getCategories());
 		model.addAttribute("conditions", this.listingService.getConditions());
 		model.addAttribute("deliveries", this.listingService.getDeliveries());
